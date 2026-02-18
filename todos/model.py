81dd3a7ec7073f18b101d49ff11320920cdad563
LIM_K_M <navymook7608@naver.com>
2026-02-18 09:12:02 +0000

    P.84

--- todos/model.py
+++ todos/model.py
@@ -1,16 +1,29 @@
+from typing import List, Optional
+
+from fastapi import Form
 from pydantic import BaseModel
 
+
 class Todo(BaseModel):
-    id: int
+    id: Optional[int]
     item: str
+
+    @classmethod
+    def as_form(
+        cls,
+        item: str = Form(...)
+    ):
+        return cls(item=item)
+
 
     class Config:
         schema_extra = {
             "example": {
                 "id": 1,
-                "item": "Example Schema!"
+                "item": "Example schema!"
             }
         }
+
 
 class TodoItem(BaseModel):
     item: str
@@ -18,6 +31,24 @@ class TodoItem(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "item": "Read the next chapter of the book."
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
             }
         }
